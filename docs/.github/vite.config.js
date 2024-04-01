b6d44928145851d0f3202b8e0c8db0ce50df5df9
Peter <pwhelanstudio@gmail.com>
2024-04-01 14:57:29 +0100

    First Commit

---
+++ docs/.github/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/vite-deploy/",
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    }
+  }
+})
